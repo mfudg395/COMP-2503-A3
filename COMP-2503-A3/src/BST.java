9ffc1c1652567086fcc72c805a9eb116a25b699c
Matthew Fudge <Matthew Fudge@10.0.0.253>
2020-03-20 20:04:33 -0600

    Implemented adding in Comparator order

--- COMP-2503-A3/src/BST.java
+++ COMP-2503-A3/src/BST.java
@@ -4,46 +4,47 @@ import java.util.ArrayDeque;
 import java.util.Comparator;
 
 public class BST<T extends Comparable<T>> implements Iterable<T> {
-	
+
 	private BSTNode root;
 	private int size;
-	
+	private Comparator<T> c;
+
 	class BSTNode implements Comparable<BSTNode> {
-		
+
 		private T data;
 		private BSTNode left;
 		private BSTNode right;
-		
+
 		public BSTNode(T data) {
 			this.data = data;
 			setLeft(null);
-			setRight(null);	
+			setRight(null);
 		}
-		
+
 		public void setLeft(BSTNode l) {
 			this.left = l;
 		}
-		
+
 		public void setRight(BSTNode r) {
 			this.right = r;
 		}
-		
+
 		public void setData(T d) {
 			data = d;
 		}
-		
+
 		public T getData() {
 			return data;
 		}
-		
+
 		public BSTNode getLeft() {
 			return left;
 		}
-		
+
 		public BSTNode getRight() {
 			return right;
 		}
-		
+
 		public boolean isLeaf() {
 			return left == null && right == null;
 		}
@@ -52,7 +53,7 @@ public class BST<T extends Comparable<T>> implements Iterable<T> {
 		public int compareTo(BSTNode node) {
 			return this.getData().compareTo(node.getData());
 		}
-		
+
 		public boolean equals(BSTNode node) {
 			return this.getData().equals(node.getData());
 		}
@@ -62,13 +63,15 @@ public class BST<T extends Comparable<T>> implements Iterable<T> {
 	public BST() {
 		root = null;
 		size = 0;
+		c = null;
 	}
 
-	public BST( Comparator<T> c) {
-		// TODO Auto-generated method stub	
-		// Create a new BST using the ordering determined by c
+	public BST(Comparator<T> c) {
+		root = null;
+		size = 0;
+		this.c = c;
 	}
-	
+
 	public void add(T d) {
 		BSTNode nodeToAdd = new BSTNode(d);
 		if (root == null) {
@@ -78,34 +81,52 @@ public class BST<T extends Comparable<T>> implements Iterable<T> {
 		}
 		size++;
 	}
-	
+
 	private void add(BSTNode root, BSTNode nodeToAdd) {
-		int compare = nodeToAdd.compareTo(root);
-		if (compare < 0) {
-			if (root.getLeft() == null) {
-				root.setLeft(nodeToAdd);
-			} else {
-				add(root.getLeft(), nodeToAdd);
+		if (c == null) {
+			int compare = nodeToAdd.compareTo(root);
+			if (compare < 0) {
+				if (root.getLeft() == null) {
+					root.setLeft(nodeToAdd);
+				} else {
+					add(root.getLeft(), nodeToAdd);
+				}
+			} else if (compare > 0) {
+				if (root.getRight() == null) {
+					root.setRight(nodeToAdd);
+				} else {
+					add(root.getRight(), nodeToAdd);
+				}
 			}
-		} else if (compare > 0) {
-			if (root.getRight() == null) {
-				root.setRight(nodeToAdd);
-			} else {
-				add(root.getRight(), nodeToAdd);
+		} else {
+			int compare = c.compare(nodeToAdd.getData(), root.getData());
+			if (compare < 0) {
+				if (root.getLeft() == null) {
+					root.setLeft(nodeToAdd);
+				} else {
+					add(root.getLeft(), nodeToAdd);
+				}
+			} else if (compare > 0) {
+				if (root.getRight() == null) {
+					root.setRight(nodeToAdd);
+				} else {
+					add(root.getRight(), nodeToAdd);
+				}
 			}
 		}
-		
+
 	}
+
 	// Help from Nahuel Paladino.
 	public BSTNode find(T data) {
 		return find(data, root);
 	}
-	
+
 	private BSTNode find(T data, BSTNode root) {
 		if (root == null) {
 			return null;
 		}
-		
+
 		int compare = data.compareTo(root.getData());
 		if (compare == 0) {
 			return root;
@@ -117,11 +138,11 @@ public class BST<T extends Comparable<T>> implements Iterable<T> {
 			return null;
 		}
 	}
-	
+
 	public void delete(T t) {
 		delete(t, root);
 	}
-	
+
 	// Completed with help from Dominic Silvestre.
 	private BSTNode delete(T t, BSTNode root) {
 		int compare = t.compareTo(root.getData());
@@ -147,23 +168,28 @@ public class BST<T extends Comparable<T>> implements Iterable<T> {
 		}
 		return root;
 	}
-	
+
 	public void inOrderAdd(BSTNode root, Queue<T> queue) {
+		if (this.root == null) {
+			this.root = root;
+			return;
+		}
+		
 		if (root.getLeft() != null) {
 			inOrderAdd(root.getLeft(), queue);
 		}
-		
+
 		queue.add(root.getData());
 
 		if (root.getRight() != null) {
 			inOrderAdd(root.getRight(), queue);
 		}
 	}
-	
+
 	public int size() {
 		return size;
 	}
-	
+
 	public BSTNode getRoot() {
 		return root;
 	}
@@ -190,11 +216,12 @@ public class BST<T extends Comparable<T>> implements Iterable<T> {
 	@Override
 	public Iterator<T> iterator() {
 		return new Iterator<T>() {
-			Queue<T> nodes; {
+			Queue<T> nodes;
+			{
 				nodes = new ArrayDeque<>();
 				inOrderAdd(root, nodes);
 			}
-			
+
 			@Override
 			public boolean hasNext() {
 				return nodes.size() != 0;
@@ -206,30 +233,30 @@ public class BST<T extends Comparable<T>> implements Iterable<T> {
 			}
 		};
 	}
-	
+
 	class BSTIterator implements Visit<Object> {
 
 		@Override
 		public void visit(Object t) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
 
 	public T min() {
 		return min(root);
 	}
-	
+
 	private T min(BSTNode root) {
 		if (root == null) {
 			return null;
 		}
-		
+
 		if (root.getLeft() != null) {
 			return min(root.getLeft());
 		}
-		
+
 		return root.getData();
 	}
 }
